de99959c63879a9629a546f8ed0a34649bd3d529
soultreemk2 <50647833+soultreemk2@users.noreply.github.com>
2019-08-13 13:44:05 +0900

    Delete assignment2.2.py

--- assignment2.2.py
+++
@@ -1,9 +0,0 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> #2.2 Write a program that uses raw_input to prompt a user for their name and then welcomes them. Note that raw_input will pop a dialog box. Enter Sarah in the pop-up box when you are prompted so your output will match the desired output below.
-
->>> name = input("Enter your name")
-Enter your name yang
->>> print ("Hello",name)
-Hello  yang
->>> 
